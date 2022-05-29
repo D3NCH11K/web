2128ba3dc1da533679d74cbc5aedd959a5879325
D3NCH11K <92727555+D3NCH11K@users.noreply.github.com>
2022-05-29 18:32:00 +0300

    Update and rename 12.1 to 12.1.js

--- web12/12.1.js
+++ web12/12.1.js
@@ -1,15 +1,24 @@
 let age = prompt("Укажите возраст", "0");
 
 let ageGroup;
-if(age <= 1){
+if(age <= 1)
+{
     ageGroup = "Щенки";
-} else {
-    if(age > 1 && age <= 3){
+} 
+else 
+{
+    if(age > 1 && age <= 3)
+    {
         ageGroup = "Молодые собаки";
-    } else {
-        if(age > 3 && age <= 7){
+    } 
+    else 
+    {
+        if(age > 3 && age <= 7)
+        {
             ageGroup = "Собаки средних лет";
-        } else {
+        } 
+        else 
+        {
             ageGroup = "Почтенные таксоны";
         }
     }
